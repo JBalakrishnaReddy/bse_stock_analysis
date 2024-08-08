137096be37308c78b8b6224ef35aa316841a11ee
Balakrishna Reddy <balakrishnareddy@Balakrishnas-MacBook-Air.local>
2024-08-08 15:12:26 +0530

    daily update

---
+++ update_stocks.py
@@ -0,0 +1,9 @@
+
+from bs4 import BeautifulSoup4
+import pandas as pd
+import numpy as np
+import json 
+
+
+"""This program contains function calls to get latest scrips listed on bse using Bhavcopy"""
+
