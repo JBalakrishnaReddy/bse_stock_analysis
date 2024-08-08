dce461a5d91609ae6ff1474dc2bde5b5012ac824
Balakrishna Reddy <balakrishnareddy@Balakrishnas-MacBook-Air.local>
2024-08-08 17:39:38 +0530

    Revert "daily update"
    This reverts commit 137096be37308c78b8b6224ef35aa316841a11ee.
    
     Changes to be committed:
    	deleted:    4666 Indian Stock Details.xlsx
    	deleted:    links.json
    	deleted:    scratch.ipynb
    	deleted:    update_stocks.py

--- update_stocks.py
+++
@@ -1,9 +0,0 @@
-
-from bs4 import BeautifulSoup4
-import pandas as pd
-import numpy as np
-import json 
-
-
-"""This program contains function calls to get latest scrips listed on bse using Bhavcopy"""
-
